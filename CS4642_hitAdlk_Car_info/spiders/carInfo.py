53fe688726b2f4a4c3532513f6c0ae5829b7cf60
unknown <lahiruk.14@cse.mrt.ac.lk>
2018-06-20 18:45:16 +0530

    create json

--- CS4642_hitAdlk_Car_info/spiders/carInfo.py
+++ CS4642_hitAdlk_Car_info/spiders/carInfo.py
@@ -8,15 +8,32 @@ class CarinfoSpider(scrapy.Spider):
         urls = [
             'http://www.hitad.lk/EN/cars?page=0',
         ]
-        for i in range(1,4):
+        for i in range(1,239):
             urls.append('http://www.hitad.lk/EN/cars?page=' + str(i*25))
             
         for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+            yield scrapy.Request(url = url, callback = self.parse)
 
     def parse(self, response):
-        page = response.url.split("/")[-1][10:]
-        filename = 'CarInfo-page-%s.html' % str(int(int(page)/25) + 1)
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log('Saved file %s' % filename)
+        for car in response.css('ul.cat-ads'):
+            link = car.css('div.clearfix a::attr(href)').extract()[0]
+            yield scrapy.Request(url = link, callback = self.parse2)
+
+
+    def parse2(self, response):
+        for line in response.css('div.fw_b'):
+            data = line.css('div.col-lg-12::text').extract()
+            yield{
+                'Published By' : data[0],
+                'Sale Type': data[1],
+                'Location': data[2],
+                'Sub Category' : data[3],
+                'Brand': data[4],
+                'Model': data[5],
+                'Transmission Type' : data[6],
+                'Year': data[7],
+                'Fuel Type': data[8],
+            }
+
+        
+        
